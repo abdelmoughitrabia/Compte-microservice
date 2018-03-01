be2f3994bb60693a416a018059114969646c2954
abdelmoughitrabia <abdelmoughit.rabia@usmba.ac.ma>
2018-03-01 17:47:00 +0000

    Externalisation de la config vers un service de configuration

--- src/main/java/com/abrab/comptemicroservice/CompteMicroserviceApplication.java
+++ src/main/java/com/abrab/comptemicroservice/CompteMicroserviceApplication.java
@@ -3,12 +3,16 @@ package com.abrab.comptemicroservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+//@EnableBinding(Sink.class)
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients("com.abrab.comptemicroservice.serviceProxy")
+@EnableCircuitBreaker
 public class CompteMicroserviceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
@@ -22,3 +26,11 @@ public class CompteMicroserviceApplication implements CommandLineRunner {
 
     }
 }
+
+//@MessageEndpoint
+//class CompteMessageAccepter {
+//
+//    public void accept(String s) {
+//        System.out.println("received msg===>" + s);
+//    }
+//}
